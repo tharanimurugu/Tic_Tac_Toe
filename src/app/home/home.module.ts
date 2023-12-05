cf39364a61c1af2615791f83e694b65204a022b0
tharanimurugu <152959765+tharanimurugu@users.noreply.github.com>
2023-12-05 15:48:09 +0530

    initial commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SquareComponent } from '../square/square.component'
+import { HomePage } from './home.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage,
+      }
+    ])
+  ],
+  declarations: [HomePage,SquareComponent]
+})
+export class HomePageModule {}
